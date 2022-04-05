df12d17e083138c7db5d52585c3c3dfb881b2c15
imskanand <the.anand3099@gmail.com>
2022-04-05 09:48:03 +0530

    Dynamic Value insertion in card

--- src/App.js
+++ src/App.js
@@ -3,20 +3,41 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero'
 import Card from './components/Card'
 import katie from './images/katie-zafers.png'
+import wedding from './images/wedding-photography.png'
+import mountain from './images/mountain-bike.png'
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
-      <Card
-        img={katie}
-        name="katie zafers"
-        rating='5.0'
-        people='6'
-        text='life lesson from katie zafers'
-        price='136'
-      />
+      <div className='card--layout'>
+        <Card
+          img={ katie }
+          name="katie zafers"
+          rating='5.0'
+          people='6'
+          text='life lesson from katie zafers'
+          price='136'
+        />
+        <Card
+          img={wedding}
+          name='wedding-photography'
+          rating='5.0'
+          people='30'
+          text='Learn wedding photography'
+          price='125'
+        />
+        <Card
+          img={ mountain }
+          name='mountain-bike'
+          rating='4.8'
+          people='2'
+          text='Group mountain biking'
+          price='50'
+        />
+      </div>
+
     </div>
 
   )
