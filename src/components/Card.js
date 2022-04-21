3794d35654165f2862ee391717151cccca276a93
imskanand <the.anand3099@gmail.com>
2022-04-21 09:30:13 +0530

    Spread Operator for object

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,23 +4,23 @@ import '../App.css'
 export default function Card(props) {
   console.log(props)
   let badgeText
-  if (props.item.openSpots === 0) {
+  if (props.openSpots === 0) {
     badgeText = "SOLD OUT"
-  } else if (props.item.location === "Online") {
+  } else if (props.location === "Online") {
     badgeText = "ONLINE"
   }
   return (
     <div className='card'>
-      { badgeText && <div className='card--badge'>{badgeText}</div>}
-      <img src={`../images/${props.item.coverImg}`} alt='card' className='card--image'/>
+      { badgeText && <div className='card--badge'>{ badgeText }</div> }
+      <img src={ props.coverImg } alt='card' className='card--image' />
       <div className='card--stats'>
-        {props.item.star && <img src={ star } alt='star' className='card--star'/>}
-        {props.item.stats.rating && <span>{ props.item.stats.rating }</span>}
-        {props.item.stats.reviewCount && <span className='grey'>({props.item.stats.reviewCount}) •</span>}
-        <span className='grey'>{ props.item.location}</span>
+        { props.star && <img src={ star } alt='star' className='card--star' /> }
+        { props.stats.rating && <span>{ props.stats.rating }</span> }
+        { props.stats.reviewCount && <span className='grey'>({ props.stats.reviewCount }) •</span> }
+        <span className='grey'>{ props.location }</span>
       </div>
-      {props.item.title && <p>{ props.item.title }</p>}
-      {props.item.price && <p><span className='bold'>From ${ props.item.price } </span>/person</p>}
+      { props.title && <p>{ props.title }</p> }
+      { props.price && <p><span className='bold'>From ${ props.price } </span>/person</p> }
     </div>
   )
 }
